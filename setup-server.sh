33cf90c59865d7c990b79e96c5e9e3ba8d7eb0d1
Alshaymaahamdan <alshaymaa.aref@hotmail.com>
2026-01-18 01:05:27 +0300

    edit

---
+++ setup-server.sh
@@ -0,0 +1,13 @@
+#install docker
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
