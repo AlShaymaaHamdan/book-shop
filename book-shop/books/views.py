7e4bbeb62f6b4cf3964cee45fccf10bc498a2ba9
Alshaymaahamdan <alshaymaa.aref@hotmail.com>
2026-01-17 22:33:46 +0300

    edit setting and views

--- book-shop/books/views.py
+++ book-shop/books/views.py
@@ -3,12 +3,12 @@ from django.views.generic import ListView , DetailView , TemplateView
 from .models import Book
 
 class BookListView(ListView):
-    template_name = 'book_list.html'
+    template_name = 'books/book_list.html'
     model= Book
-    context_object_name = 'book_list'
+    context_object_name = 'books/book_list'
 
 class BookDetailView(DetailView):
-    template_name = 'book_detail.html'
+    template_name = 'books/book_detail.html'
     model = Book
 
 class BookTemplateView(TemplateView):
