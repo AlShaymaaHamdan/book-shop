16e6b67deea78f38524b6f38c0e4328ce378cb73
Alshaymaahamdan <alshaymaa.aref@hotmail.com>
2026-01-19 17:52:55 +0300

    add promote and deploy

--- setup-server.sh
+++ setup-server.sh
@@ -4,10 +4,32 @@ sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ubuntu
 
 #install AWS CLI
+sudo apt install curl -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
+
+#install nginx
+sudo apt update
+sudo apt install nginx
+
+#install microk8s
+sudo snap install microk8s --classic
+sudo usermod -a -G microk8s ubuntu
+newgrp microk8s
+microk8s status --wait-ready
+alias k="microk8s kubectl"
+k get all --all-namespaces
